f2c0cde8759f0b0267be2a15e84ad0cf2d58d37f
doumbia seybou <doumbiaseyboudiokolo@gmail.com>
2023-04-19 15:37:24 +0200

    la modif

--- style-switcher.js
+++ style-switcher.js
@@ -3,6 +3,7 @@ const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
+
 // hide style
 window.addEventListener("scroll", () => {
     if(document.querySelector(".style-switcher").classList.contains("open"))
@@ -24,19 +25,19 @@ function setActiveStyle(color)
     })
 }
 
-const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener("click", () => {
-    dayNight.querySelectorAll("i").classList.toggle("fa-sun");
-    dayNight.querySelectorAll("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
-})
-window.addEventListener("load", () => {
-    if(document.body.classList.contains("dark"))
-    {
-        dayNight.querySelectorAll("i").classList.add("fa-sun");
-    }
-    else
-    {
-        dayNight.querySelectorAll("i").classList.add("fa-moon");
-    }
-})
+// const dayNight = document.querySelector(".day-night");
+// dayNight.addEventListener("click", () => {
+//     dayNight.querySelectorAll("i").classList.toggle("fa-sun");
+//     dayNight.querySelectorAll("i").classList.toggle("fa-moon");
+//     document.body.classList.toggle("dark");
+// })
+// window.addEventListener("load", () => {
+//     if(document.body.classList.contains("dark"))
+//     {
+//         dayNight.querySelectorAll("i").classList.add("fa-sun");
+//     }
+//     else
+//     {
+//         dayNight.querySelectorAll("i").classList.add("fa-moon");
+//     }
+// })
