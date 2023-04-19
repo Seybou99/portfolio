3aae1f915d679e203bc04d42dbe9a465814fdb05
doumbia seybou <doumbiaseyboudiokolo@gmail.com>
2023-04-19 13:43:12 +0200

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Etudiant", "Développeur Web", "& Mobile"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop: true
+})
---
+++ style-switcher.js
@@ -0,0 +1,42 @@
+/*toggle*/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+// hide style
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+const alternateStyles = document.querySelectorAll('.alternate-style');
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelectorAll("i").classList.toggle("fa-sun");
+    dayNight.querySelectorAll("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelectorAll("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelectorAll("i").classList.add("fa-moon");
+    }
+})
